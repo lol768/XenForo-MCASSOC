4e2d3fe098aed38fccea356e088cf39fc5f5b61d
lol768 <lol768@lol768.com>
2015-02-26 16:39:33 +0000

    Fix up AJAX thread replies and conversation replies not showing association info.
    Closes #7.

--- ControllerPublic/PrivateMessage.php
+++ ControllerPublic/PrivateMessage.php
@@ -7,22 +7,20 @@
 //class AssociationMc_ControllerPublic_PrivateMessage extends XenForo_ControllerPublic_Conversation {
 class AssociationMc_ControllerPublic_PrivateMessage extends XFCP_AssociationMc_ControllerPublic_PrivateMessage {
 
+    /**
+     * Handles viewing a conversation page.
+     * @return mixed
+     */
     public function actionView() {
         $view = parent::actionView();
-        if (!$view instanceof XenForo_ControllerResponse_View) {
-            return $view;
-        }
-        $uniqueUserIds = $this->getUniqueUserIds($view->params['messages']);
-        $model = $this->_getAssociationEntryModel();
-        $entries = $model->getEntriesByUserIds($uniqueUserIds, true);
-        $names = [];
-        foreach ($entries as $entry) {
-            $names[$entry['xenforo_id']] = $entry['last_username'];
-        }
-        $view->params['mcNames'] = $names;
-        return $view;
+        return $this->transformView($view);
     }
 
+    /**
+     * Gets all the unique user IDs for a bunch of messages.
+     * @param array $messages The messages to scan.
+     * @return int[] User id array.
+     */
     private function getUniqueUserIds(array $messages) {
         $ids = [];
         foreach ($messages as $postId => $post) {
@@ -38,6 +36,34 @@ class AssociationMc_ControllerPublic_PrivateMessage extends XFCP_AssociationMc_C
      */
     private function _getAssociationEntryModel() {
         return $this->getModelFromCache('AssociationMc_Model_AssociationEntry');
+    }
+
+    /**
+     * Handles the AJAX replies.
+     * @return mixed
+     */
+    public function actionInsertReply() {
+        $view = parent::actionInsertReply();
+        return $this->transformView($view);
+    }
+
+    /**
+     * @param XenForo_ControllerResponse_Abstract $view
+     * @return mixed The modified view.
+     */
+    private function transformView($view) {
+        if (!$view instanceof XenForo_ControllerResponse_View) {
+            return $view;
+        }
+        $uniqueUserIds = $this->getUniqueUserIds($view->params['messages']);
+        $model = $this->_getAssociationEntryModel();
+        $entries = $model->getEntriesByUserIds($uniqueUserIds, true);
+        $names = [];
+        foreach ($entries as $entry) {
+            $names[$entry['xenforo_id']] = $entry['last_username'];
+        }
+        $view->params['mcNames'] = $names;
+        return $view;
     }
 
 } 
--- ControllerPublic/Thread.php
+++ ControllerPublic/Thread.php
@@ -7,8 +7,37 @@
 //class AssociationMc_ControllerPublic_Thread extends XenForo_ControllerPublic_Thread {
 class AssociationMc_ControllerPublic_Thread extends XFCP_AssociationMc_ControllerPublic_Thread {
 
+    /**
+     * Used when just viewing a thread.
+     * @return mixed
+     */
     public function actionIndex() {
         $view = parent::actionIndex();
+        return $this->transformView($view);
+
+    }
+
+    /**
+     * @return AssociationMc_Model_AssociationEntry
+     */
+    private function _getAssociationEntryModel() {
+        return $this->getModelFromCache('AssociationMc_Model_AssociationEntry');
+    }
+
+    /**
+     * Used when displaying posts after submitting a new rpely.
+     * @return mixed
+     */
+    public function actionAddReply() {
+        $view = parent::actionAddReply();
+        return $this->transformView($view);
+    }
+
+    /**
+     * @param XenForo_ControllerResponse_Abstract $view
+     * @return mixed The modified view.
+     */
+    private function transformView($view) {
         $uniqueUserIds = [];
         if (!$view instanceof XenForo_ControllerResponse_View) {
             return $view;
@@ -26,14 +55,6 @@ class AssociationMc_ControllerPublic_Thread extends XFCP_AssociationMc_Controlle
         }
         $view->params['mcNames'] = $names;
         return $view;
-
-    }
-
-    /**
-     * @return AssociationMc_Model_AssociationEntry
-     */
-    private function _getAssociationEntryModel() {
-        return $this->getModelFromCache('AssociationMc_Model_AssociationEntry');
     }
 
 } 
