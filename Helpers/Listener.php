a23bc0de6f77261f2eaca228f150187acf008b22
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-04-13 17:44:21 -0400

    Update Listener.php

--- Helpers/Listener.php
+++ Helpers/Listener.php
@@ -5,7 +5,8 @@ class AssociationMc_Helpers_Listener {
     public static function init(XenForo_Dependencies_Abstract $dependencies, array $data) {
         // Register helper to grab the URL for the skin head image given a size and username
         XenForo_Template_Helper_Core::$helperCallbacks += array(
-            'headimage' => array('AssociationMc_Helpers_HeadImage', 'helperGetHeadImageUrl')
+            'headimage' => array('AssociationMc_Helpers_HeadImage', 'helperGetHeadImageUrl'),
+            'additionalinfo' => array('AssociationMc_Helpers_AdditionalInfo', 'helperGetAdditionalInfoUrl')
         );
     }
 
