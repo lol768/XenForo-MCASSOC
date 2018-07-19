ff5a0608384893b16787f948db942ccfd41dde1f
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-07-19 16:37:04 -0400

    Update ThreadPost.php

--- Listener/ThreadPost.php
+++ Listener/ThreadPost.php
@@ -5,7 +5,7 @@ class AssociationMc_Listener_ThreadPost {
     public static function hook($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template) {
 
         /** @var AssociationMc_Model_AssociationEntry $model */
-        $model = XenForo_Model::create("AssociationMc_Model_ThreadPost");
+        $model = XenForo_Model::create("AssociationMc_Model_AssociationEntry");
         $entries = $model->getEntryCountForUserId($hookParams['user']['user_id']);
         if ($entries > 0) {
             $myTemplate = new XenForo_Template_Public("association_thread_post", $hookParams);
