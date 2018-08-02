485db4b4b442a498ea3cd63139ea49d7ad5f80f9
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-08-02 19:41:45 -0400

    Reverted Param Move

--- Listener/ThreadPost.php
+++ Listener/ThreadPost.php
@@ -2,6 +2,7 @@
 class AssociationMc_Listener_ThreadPost {
     public static function hook($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template) {
         $myTemplate = new XenForo_Template_Public("association_thread_post", $hookParams);
+        $myTemplate->setParam("addInfo", XenForo_Application::getOptions()->mcAssocAddInfo);
         $contents .= $myTemplate->render();
     }
 }
